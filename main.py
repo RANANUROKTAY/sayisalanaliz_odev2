e1c3153f34ff286ab57996b81d154140fe8d1254
RANANUROKTAY <136755355+RANANUROKTAY@users.noreply.github.com>
2023-11-18 02:33:18 +0300

    Update main.py
    Hocam ödevlerimin klasör adları farklı olsada hepsinin adını main koymuşum. Sisteme yüklediğimde sadece 1  tanesi vardı. Ben de onun içine girip diğer iki kodumu da aktardım. Hepsi tek bir 'main' sayfası içinde yer alıyor.

--- main.py
+++ main.py
@@ -1,3 +1,261 @@
+#02220224004 - RANA NUR OKTAY - İLK ÖDEV
+#İLK SORU
+
+x_alt=2
+print("Alt kök=",x_alt)
+denklem_alt=x_alt**3 - 2*x_alt**2 - 5
+print("Alt kökün degeri=",denklem_alt)
+
+x_ust=4
+print("Üst kök=",x_ust)
+denklem_ust=x_ust**3 - 2*x_ust**2 - 5
+print("Üst kökün degeri=",denklem_ust)
+
+
+x_kok=(x_alt+x_ust)/2
+print("İkiye bölünmüş kök=",x_kok)
+denklem_boluk=x_kok**3 - 2*x_kok**2 - 5
+print("İkiye bölünmüş kökün değeri=",denklem_boluk)
+print("Yeni kök değerleri=")
+if  denklem_alt * denklem_boluk<0:
+        x_alt=x_alt
+        x_ust=x_kok
+        print(x_alt,"  ",x_ust)
+
+
+elif denklem_ust*denklem_boluk<0 :
+        x_alt=x_kok
+        x_ust=x_ust
+        print(x_alt,"  ",x_ust)
+
+else :
+    print("Denklem çözümü tıkandı. ")
+
+
+
+print("Alt kök=",x_alt)
+denklem_alt=x_alt**3 - 2*x_alt**2 - 5
+print("Alt kökün degeri=",denklem_alt)
+
+
+print("Üst kök=",x_ust)
+denklem_ust=x_ust**3 - 2*x_ust**2 - 5
+print("Üst kökün degeri=",denklem_ust)
+
+
+x_kok=(x_alt+x_ust)/2
+print("İkiye bölünmüş kök=",x_kok)
+denklem_boluk=x_kok**3 - 2*x_kok**2 - 5
+print("İkiye bölünmüş kökün değeri=",denklem_boluk)
+
+print("Yeni kök değerleri=")
+if  denklem_alt * denklem_boluk<0:
+        x_alt=x_alt
+        x_ust=x_kok
+        print(x_alt,"  ",x_ust)
+
+
+elif denklem_ust*denklem_boluk<0 :
+        x_alt=x_kok
+        x_ust=x_ust
+        print(x_alt,"  ",x_ust)
+
+else :
+    print("Denklem çözümü tıkandı. ")
+
+
+
+print("Alt kök=",x_alt)
+denklem_alt=x_alt**3 - 2*x_alt**2 - 5
+print("Alt kökün degeri=",denklem_alt)
+
+
+print("Üst kök=",x_ust)
+denklem_ust=x_ust**3 - 2*x_ust**2 - 5
+print("Üst kökün degeri=",denklem_ust)
+
+
+x_kok=(x_alt+x_ust)/2
+print("İkiye bölünmüş kök=",x_kok)
+denklem_boluk=x_kok**3 - 2*x_kok**2 - 5
+print("İkiye bölünmüş kökün değeri=",denklem_boluk)
+
+print("Yeni kök değerleri=")
+if  denklem_alt * denklem_boluk<0:
+        x_alt=x_alt
+        x_ust=x_kok
+        print(x_alt,"  ",x_ust)
+
+elif denklem_ust*denklem_boluk<0 :
+        x_alt=x_kok
+        x_ust=x_ust
+        print(x_alt,"  ",x_ust)
+
+else :
+    print("Denklem çözümü tıkandı. ")
+
+
+print("Alt kök=",x_alt)
+denklem_alt=x_alt**3 - 2*x_alt**2 - 5
+print("Alt kökün degeri=",denklem_alt)
+
+
+print("Üst kök=",x_ust)
+denklem_ust=x_ust**3 - 2*x_ust**2 - 5
+print("Üst kökün degeri=",denklem_ust)
+
+
+x_kok=(x_alt+x_ust)/2
+print("İkiye bölünmüş kök=",x_kok)
+denklem_boluk=x_kok**3 - 2*x_kok**2 - 5
+print("İkiye bölünmüş kökün değeri=",denklem_boluk)
+
+print("Yeni kök değerleri=")
+if  denklem_alt * denklem_boluk<0:
+        x_alt=x_alt
+        x_ust=x_kok
+        print(x_alt,"  ",x_ust)
+
+elif denklem_ust*denklem_boluk<0 :
+        x_alt=x_kok
+        x_ust=x_ust
+        print(x_alt,"  ",x_ust)
+
+else :
+    print("Denklem çözümü tıkandı. ")
+
+
+#İKİNCİ SORU
+
+x_alt=1
+print("Alt kök=",x_alt)
+denklem_alt=x_alt**3 + 4*x_alt**2 - 10
+print("Alt kökün degeri=",denklem_alt)
+
+x_ust=2
+print("Üst kök=",x_ust)
+denklem_ust=x_ust**3 + 4*x_ust**2 - 10
+print("Üst kökün degeri=",denklem_ust)
+
+
+x_kok=(x_alt+x_ust)/2
+print("İkiye bölünmüş kök=",x_kok)
+denklem_boluk=x_kok**3 + 4*x_kok**2 - 10
+print("İkiye bölünmüş kökün değeri=",denklem_boluk)
+print("Yeni kök değerleri=")
+if  denklem_alt * denklem_boluk<0:
+        x_alt=x_alt
+        x_ust=x_kok
+        print(x_alt,"  ",x_ust)
+
+
+elif denklem_ust*denklem_boluk<0 :
+        x_alt=x_kok
+        x_ust=x_ust
+        print(x_alt,"  ",x_ust)
+
+else :
+    print("Denklem çözümü tıkandı. ")
+
+
+
+
+print("Alt kök=",x_alt)
+denklem_alt=x_alt**3 + 4*x_alt**2 - 10
+print("Alt kökün degeri=",denklem_alt)
+
+
+print("Üst kök=",x_ust)
+denklem_ust=x_ust**3 + 4*x_ust**2 - 10
+print("Üst kökün degeri=",denklem_ust)
+
+
+x_kok=(x_alt+x_ust)/2
+print("İkiye bölünmüş kök=",x_kok)
+denklem_boluk=x_kok**3 + 4*x_kok**2 - 10
+print("İkiye bölünmüş kökün değeri=",denklem_boluk)
+
+print("Yeni kök değerleri=")
+if  denklem_alt * denklem_boluk<0:
+        x_alt=x_alt
+        x_ust=x_kok
+        print(x_alt,"  ",x_ust)
+
+
+elif denklem_ust*denklem_boluk<0 :
+        x_alt=x_kok
+        x_ust=x_ust
+        print(x_alt,"  ",x_ust)
+
+else :
+    print("Denklem çözümü tıkandı. ")
+
+
+
+print("Alt kök=",x_alt)
+denklem_alt=x_alt**3 + 4*x_alt**2 - 10
+print("Alt kökün degeri=",denklem_alt)
+
+
+print("Üst kök=",x_ust)
+denklem_ust=x_ust**3 + 4*x_ust**2 - 10
+print("Üst kökün degeri=",denklem_ust)
+
+
+x_kok=(x_alt+x_ust)/2
+print("İkiye bölünmüş kök=",x_kok)
+denklem_boluk=x_kok**3 + 4*x_kok**2 - 10
+print("İkiye bölünmüş kökün değeri=",denklem_boluk)
+
+print("Yeni kök değerleri=")
+if  denklem_alt * denklem_boluk<0:
+        x_alt=x_alt
+        x_ust=x_kok
+        print(x_alt,"  ",x_ust)
+
+
+elif denklem_ust*denklem_boluk<0 :
+        x_alt=x_kok
+        x_ust=x_ust
+        print(x_alt,"  ",x_ust)
+
+else :
+    print("Denklem çözümü tıkandı. ")
+
+
+print("Alt kök=",x_alt)
+denklem_alt=x_alt**3 + 4*x_alt**2 - 10
+print("Alt kökün degeri=",denklem_alt)
+
+
+print("Üst kök=",x_ust)
+denklem_ust=x_ust**3 + 4*x_ust**2 - 10
+print("Üst kökün degeri=",denklem_ust)
+
+
+x_kok=(x_alt+x_ust)/2
+print("İkiye bölünmüş kök=",x_kok)
+denklem_boluk=x_kok**3 + 4*x_kok**2 - 10
+print("İkiye bölünmüş kökün değeri=",denklem_boluk)
+
+print("Yeni kök değerleri=")
+if  denklem_alt * denklem_boluk<0:
+        x_alt=x_alt
+        x_ust=x_kok
+        print(x_alt,"  ",x_ust)
+
+
+elif denklem_ust*denklem_boluk<0 :
+        x_alt=x_kok
+        x_ust=x_ust
+        print(x_alt,"  ",x_ust)
+
+else :
+    print("Denklem çözümü tıkandı. ")
+
+
+#ÜÇÜNCÜ SORU
+
 import math
 print("Birinci iterasyon degeri=")
 x=2
@@ -19,3 +277,5 @@ x=x-(denklem/turevi)
 denklem=4*math.e**(-0.5*x)-x
 turevi=-2*math.e**(-0.5*x)-1
 print(x-(denklem/turevi))
+
+
