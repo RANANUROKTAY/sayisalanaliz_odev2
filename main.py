12160b2eed5e62e4f40db606f879411fc39c8a1e
RANANUROKTAY <136755355+RANANUROKTAY@users.noreply.github.com>
2023-11-18 10:04:25 +0300

    Update main.py
    Ödev dosyasının içinde 3 soru da mevcuttur.

--- main.py
+++ main.py
@@ -281,3 +281,28 @@ turevi=-2*e**(-0.5*x)-1
 print(x-(denklem/turevi))
 
 
+print("\n\n")
+
+#Kütüphane kullanılmış hali
+
+import math
+print("Birinci iterasyon degeri=")
+x=2
+denklem=4*math.e**(-0.5*x)-x
+turevi=-2*math.e**(-0.5*x)-1
+print(x-(denklem/turevi))
+print("İkinci iterasyon degeri=")
+x=x-(denklem/turevi)
+denklem=4*math.e**(-0.5*x)-x
+turevi=-2*math.e**(-0.5*x)-1
+print(x-(denklem/turevi))
+print("Üçüncü iterasyon degeri=")
+x=x-(denklem/turevi)
+denklem=4*math.e**(-0.5*x)-x
+turevi=-2*math.e**(-0.5*x)-1
+print(x-(denklem/turevi))
+print("Dördüncü iterasyon degeri=")
+x=x-(denklem/turevi)
+denklem=4*math.e**(-0.5*x)-x
+turevi=-2*math.e**(-0.5*x)-1
+print(x-(denklem/turevi))
